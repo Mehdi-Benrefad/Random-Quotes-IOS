6f4bedba97b2fed135c5fda8ceec532f2088dee7
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-14 03:32:33 +0000

    animate activity indicator

--- Random-Quotes-IOS/ViewController.swift
+++ Random-Quotes-IOS/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var newQuoteButtonOutlet: UIButton!
+    @IBOutlet weak var ActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newQuoteButtonOutlet.isHidden=true
+        ActivityIndicator.isHidden=false
+        ActivityIndicator.startAnimating()
+
+        //on fait appel a la methode statique getQuote en lui passant une fonction callback en parametres
+        QuoteService.getQuote { (success, quote) in
+            //si la requette est reussie o affiche la citation
+              if success, let quote = quote {
+                  self.update(quote: quote)
+              } else {// si la requette echoue on affiche une alerte d'erreur
+               self.presentAlert()
+              }
+          }
     }
 
+    
+    
+    
      //action sur le bouton new Quote
     @IBAction func tappedNewQuoteButton() {
+        
+        newQuoteButtonOutlet.isHidden=true
+        ActivityIndicator.isHidden=false
+        ActivityIndicator.startAnimating()
+
         //on fait appel a la methode statique getQuote en lui passant une fonction callback en parametres
          QuoteService.getQuote { (success, quote) in
              //si la requette est reussie o affiche la citation
@@ -37,9 +61,17 @@ class ViewController: UIViewController {
         quoteLabel.text = quote.text
         authorLabel.text = quote.author
         imageView.image = UIImage(data: quote.imageData)
+        
+        newQuoteButtonOutlet.isHidden=false
+        ActivityIndicator.isHidden=true
+        
     }
 
     private func presentAlert() {
+        
+        newQuoteButtonOutlet.isHidden=false
+        ActivityIndicator.isHidden=true
+
         let alertVC = UIAlertController(title: "Error", message: "The quote download failed.", preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         present(alertVC, animated: true, completion: nil)
