8f93dc50695dcd2626b11bef664f291e2768c301
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-14 01:25:14 +0000

    Initial Commit

---
+++ Random-Quotes-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random-Quotes-IOS
+//
+//  Created by Mehdi Benrefad on 14/04/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
